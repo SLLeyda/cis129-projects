12e13865c0e1452e4f9dd37000f514ea8f3031bf
SLLeyda <159022389+SLLeyda@users.noreply.github.com>
2024-02-05 15:31:54 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -3,5 +3,5 @@
 # Prints a simple short story 
 print('Once upon a time there was a teacher.')
 print('He asked a bunch of programming nerds to write a story.')
-print('But one of them didn't have any ideas.')
+print('But one of them did not have any ideas.')
 print('So he took the easy way out.')
