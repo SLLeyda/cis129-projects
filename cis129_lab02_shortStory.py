299b2f789b49c817cf8834312b0818210298c7cc
SLLeyda <159022389+SLLeyda@users.noreply.github.com>
2024-02-05 15:31:17 -0700

    Update cis129_lab02_shortStory.py
    Added story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author: Scott Leyda
+# CIS129 course Module 2 Lab
+# Prints a simple short story 
+print('Once upon a time there was a teacher.')
+print('He asked a bunch of programming nerds to write a story.')
+print('But one of them didn't have any ideas.')
+print('So he took the easy way out.')
